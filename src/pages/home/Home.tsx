217aae6f31d63e3c4a837dda19708408463c6f7c
jamuelton <jamueltonangelim@gmail.com>
2023-11-18 19:38:33 -0300

    feat: adding texts and css config to navbar layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from "../../assets/marquinhos.png"
 
 export function Home() {
   return (
@@ -6,9 +7,25 @@ export function Home() {
         <header className='bg-[#565651] flex flex-row justify-center p-1'>
             <p className='text-[#ffffff]'>Marquinhos Financiamentos</p>
         </header>
-        <nav>Barra de navegação</nav>
-        <aside>Barra lateral</aside>
-        <div>conteudo</div>
+        <nav className='bg-[#dc143c] flex flex-row justify-between'>
+            <div className='w-1/12 h-1/12 p-2'>
+                <img src={logo} alt="" />
+            </div>
+            <div className='h-1/12 flex items-center p-2'>
+               <ul className='flex flex-row justify-center items-center gap-5 text-white text-lg font-semibold'>
+                    <li className='cursor-pointer'>Carros</li>
+                    <li className='cursor-pointer'>Motos</li>
+                    <li className='cursor-pointer'>Financiar</li>
+               </ul>
+            </div>
+        </nav>
+        <section className='flex flex-row justify-center'>
+            <div className='flex flex-col justify-center  items-center mt-10 w-9/12'>
+                <h1 className='text-[#f3bc26] text-4xl font-bold'>Bem-vindo à Marquinhos Financiamentos</h1>
+                <h2 className='text-[#f3bc26] text-3xl font-bold'>Sua Jornada para o Carro ou a Moto dos Sonhos Começa Aqui!</h2>
+            </div>
+        </section>
+       
         <footer>footer</footer>
     </main>
   )
