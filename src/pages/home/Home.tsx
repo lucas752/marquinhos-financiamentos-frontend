5ab39fba5ec6f4d30c09d28785edc550157063c8
jamuelton <jamueltonangelim@gmail.com>
2023-11-18 18:26:30 -0300

    add: adding tailwind

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export function Home() {
   return (
-    <main>
-        <header>Cabeçalhos</header>
+    <main className='w-full'>
+        <header className='bg-[#565651] flex flex-row justify-center p-1'>
+            <p className='text-[#ffffff]'>Marquinhos Financiamentos</p>
+        </header>
         <nav>Barra de navegação</nav>
         <aside>Barra lateral</aside>
         <div>conteudo</div>
