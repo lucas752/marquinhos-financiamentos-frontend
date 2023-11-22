e72668335e9bab28bd8d2e840a3e16217595eefb
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2023-11-22 14:17:36 -0300

    feat: adicionando card de resultado e criando lógica para gravar os dados

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,9 @@ export default function Input(props: {
   labelText: string;
   type: string;
   min?: number;
+  max?: number;
   placeholder: string;
+  onChange: (e: { target: { value: string } }) => void;
 }) {
   return (
     <div className="flex flex-col">
@@ -12,8 +14,11 @@ export default function Input(props: {
       <input
         type={props.type}
         min={props.min}
+        max={props.max}
         placeholder={props.placeholder}
         className="focus:border-[#dc143c] focus:outline-none   border-2 p-2 rounded-3xl w-72"
+        onChange={props.onChange}
+        required
       />
     </div>
   );
